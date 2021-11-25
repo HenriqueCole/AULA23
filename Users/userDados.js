b7232012cb26df8dfeadae90a5828517e7e9ee9b
GabrielVolpi <gabriel_volpi1@estudante.sc.senai.br>
2021-11-25 20:14:31 -0300

    Feito Tudo menos os extras

--- Users/userDados.js
+++ Users/userDados.js
@@ -19,19 +19,33 @@ function showUserGithub(user) {
     if (!user) return;
     let divName = document.createElement('div');
     divName.innerText = user.login;
-    
+
+    let divUserAvatar = document.createElement('img')
+    divUserAvatar.src = user.avatar_url;
+
     let divUser = document.createElement('div');
     divUser.innerText = user.name;
 
     document.body.appendChild(divName);
     document.body.appendChild(divUser);
+    document.body.appendChild(divUserAvatar);
 }
 
-function getUserReposGithub(userName) {
+function getUserReposGithub(a) {
     fetch('https://fake-github.herokuapp.com/api/search/' + userName + '/repos')
         .then(function (resultado) {
-            resultado.json().then(function (data) {
-                console.log('Repositories Data:', data);
+            resultado.json().then(function (dataRepositorio) {
+                dataRepositorio.forEach(function (element) {
+
+                    console.log('Repositories Data:', element.name);
+                    const linha = document.createElement('div')
+                    document.body.appendChild(linha);
+                    linha.innerText = element.name;
+                    const link = document.createElement('a')
+                    link.href = element.url;
+                    link.innerText = element.url
+                    linha.appendChild(link);
+                });
             });
         }).catch(function (erro) {
             console.log('erro:', erro);
